d4fa55d77dd87e9485bc8efdf7ee479328bc6637
heroesluk <heroesluk@gmail.com>
2023-03-10 13:53:17 +0100

    For merging with other repo

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new UserLogic().getView()));
+        primaryStage.show();
+    }
+}
